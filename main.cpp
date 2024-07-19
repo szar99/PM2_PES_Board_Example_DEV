61f5b24c99f5c3824d588274ba32befcd5aef6e0
szar99 <maciekszarek@onet.pl>
2024-07-19 11:43:15 +0200

    Documentation for stepper motor driver

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include "pm2_drivers/DebounceIn.h"
 #include "pm2_drivers/Stepper.h"
 #include <chrono>
-//#include "pm2_drivers/Stepper.h"
 
 bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and
                                    // decides whether to execute the main task or not
@@ -36,21 +35,11 @@ int main()
     // led on nucleo board
     DigitalOut user_led(USER_LED);
 
-    //Stepper stepper(PB_14, PC_4, 1.0f, 200);
-    //Stepper stepper(PB_14, PC_4);
+    Stepper stepper(PB_14, PC_4);
 
-    //Stepper stepper(PB_12, PA_15, 1.0f, 200);
-    DigitalOut step(PB_14);
-    DigitalOut dir(PC_4);
     DigitalOut enableStepper(PB_1);
-    //DigitalOut cs()
-
-    //DigitalOut halfstep(PC_5);
 
     enableStepper.write(0);
-    dir.write(1);
-
-    //halfstep.write(1);
 
     // start timer
     main_task_timer.start();
@@ -59,32 +48,26 @@ int main()
         main_task_timer.reset();
 
         if (do_execute_main_task) {
-            step.write(1);
-            thread_sleep_for(1);
-            step.write(0);
-            thread_sleep_for(1);
 
-            
-            // stepper.startRotation();
-            //stepper.setAbsolutePosition(600);
+            stepper.startRotation();
         } else {
             // the following code block gets executed only once
             if (do_reset_all_once) {
                 do_reset_all_once = false;
-                // stepper.stopRotation();
-                // //stepper.setAbsoluteZeroPosition();
-                // stepper.setSpeed(-1.0f);
-                // stepper.startRotation();
+                stepper.stopRotation();
+                stepper.setAbsoluteZeroPosition();
+
+                stepper.setRelativePositon(-5000);
             }
 
         }
 
-        // printf("%f \n", stepper.getPosition());
+        printf("%f \n", stepper.getPosition());
         // toggling the user led
         user_led = !user_led;
         // read timer and make the main thread sleep for the remaining time span (non blocking)
-        // int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
-        // thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
+        int main_task_elapsed_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(main_task_timer.elapsed_time()).count();
+        thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
     }
 }
 
